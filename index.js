8cefc9b6785ff5f68dfe03714e9dec48ac7eed40
tslmn7 <126646572+tslmn7@users.noreply.github.com>
2023-05-02 13:21:12 +0000

    aaa

---
+++ index.js
@@ -0,0 +1,25 @@
+// let operator = '';
+
+// function calculate() {
+//   switch (operator) {
+//     case '+':
+//       return num1 + num2;
+//     case '-':
+//       return num1 - num2;
+//     case '*':
+//       return num1 * num2;
+//     case '/':
+//       return num1 / num2;
+//     default:
+//       return 0;
+//   }
+// }
+
+
+// num1 = parseFloat(prompt('Enter the first number:'));
+// operator = prompt('Enter an operator (+, -, *, /):');
+// num2 = parseFloat(prompt('Enter the second number:'));
+
+
+// const result = calculate();
+// alert(`${num1} ${operator} ${num2} = ${result}`);
